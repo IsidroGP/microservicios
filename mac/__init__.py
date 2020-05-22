2511b70a89674b2e667d87df0960625100293dec
IsidroGP <33144001@uaz.edu.mx>
2020-05-21 20:50:02 -0500

    Funciona punto 5, quitar mono

--- mac/__init__.py
+++ mac/__init__.py
@@ -9,12 +9,12 @@ app.config['DEBUG'] = True
 @app.route('/netflix/original-content', methods=['GET', 'POST'])
 def crud():
     render_template("hola.html")
+    sqlite_conn = get_database_connection('./mac/original_content.db')
+    sqlite_cursor = sqlite_conn.cursor()
     if request.method == 'GET':
         genero = request.args.get('gen')
         type = request.args.get('type')
         popularity = request.args.get('pop')
-        sqlite_conn = get_database_connection('./mac/original_content.db')
-        sqlite_cursor = sqlite_conn.cursor()
         get_sentence(sqlite_cursor, genero, type, popularity)
         data = sqlite_cursor.fetchall()
         result = convert_cursor_to_json(data)
@@ -23,11 +23,6 @@ def crud():
         title = request.form
         apikey = "712828a0"
         headers = {"Authorization":apikey}
-        sqlite_conn = get_database_connection('./mac/original_content.db')
-        sqlite_cursor = sqlite_conn.cursor()
-        sqlite_cursor.execute('SELECT * FROM original_content')
-        data = sqlite_cursor.fetchall()
-        result = convert_cursor_to_json(data)
         req = requests.get("http://www.omdbapi.com/?t={0}&plot=full&apikey=712828a0&".format(title['title']))
         json_body = req.json();
         create_element(sqlite_cursor, json_body)
@@ -39,30 +34,28 @@ def crud():
 
 @app.route('/netflix/original-content/<int:id>', methods=['GET','PATCH'])
 def get_sentence_id(id):
+    sqlite_conn = get_database_connection('./mac/original_content.db')
+    sqlite_cursor = sqlite_conn.cursor()
     if request.method == 'GET':
-        print("get")
-        sqlite_conn = get_database_connection('./mac/original_content.db')
-        sqlite_cursor = sqlite_conn.cursor()
         sqlite_cursor.execute("SELECT * FROM original_content WHERE id ="+str(id)+"")
         data = sqlite_cursor.fetchall()
         result = convert_cursor_to_json(data)
         return render_template('hola2.html', results = (result), datas = (result))
     if request.method == 'PATCH':
-        print("patch")
         json_body = request.get_json()
-        print(json_body)
-        print("dsadasdasd")
-        return "HOLA"
+        update_element(id,sqlite_cursor,json_body)
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE id ="+str(id)+"")
+        data = sqlite_cursor.fetchall()
+        result = convert_cursor_to_json(data)
+        sqlite_conn.commit()
+        return render_template('hola2.html', results = (result), datas = (result))
 
-def get_sentence(sqlite_cursor, genero, type, popularity):
-    if genero != None:
-        sqlite_cursor.execute("SELECT * FROM original_content WHERE genre ="+genero+"")
-    elif type != None:
-        sqlite_cursor.execute("SELECT * FROM original_content WHERE type ="+type+"")
-    elif popularity != None:
-        sqlite_cursor.execute("SELECT * FROM original_content WHERE imdb_rating ="+popularity+"")
-    else:
-        sqlite_cursor.execute("SELECT * FROM original_content")
+def update_element(id,sqlite_cursor,json_body):
+    title = json_body['Title']
+    genre = json_body['Genre']
+    type = json_body['Type']
+    imdb_rtg = json_body['imdbRag']
+    sqlite_cursor.execute("UPDATE original_content SET name = ?, type = ?,genre = ?,imdb_rating = ? WHERE id = ?",(title,type,genre,imdb_rtg,str(id)))
 
 def create_element(sqlite_cursor, json_body):
     sqlite_cursor.execute('SELECT id FROM original_content WHERE id = (SELECT MAX(id) FROM original_content)')
@@ -73,7 +66,23 @@ def create_element(sqlite_cursor, json_body):
     imdb_rtg = json_body['imdbRating']
     sqlite_cursor.execute("INSERT INTO original_content VALUES (?,?,?,?,?)",(str(id), title, type, genre, str(imdb_rtg)))
 
-
+def get_sentence(sqlite_cursor, genero, type, popularity):
+    if genero != None and type == None and popularity == None:
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE genre ="+genero+"")
+    elif type != None and genero == None and popularity == None:
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE type ="+type+"")
+    elif popularity != None and type == None and genero == None:
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE imdb_rating ="+popularity+"")
+    elif genero != None and type != None and popularity == None:
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE genre ="+genero+" and type = "+type+"")
+    elif genero != None and type == None and popularity != None:
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE genre ="+genero+" and imdb_rating = "+popularity+"")
+    elif genero == None and type != None and popularity != None:
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE imdb_rating ="+popularity+" and type = "+type+"")
+    elif genero != None and type != None and popularity != None:
+        sqlite_cursor.execute("SELECT * FROM original_content WHERE imdb_rating ="+popularity+" and type = "+type+" and genre = "+genero+"")
+    else:
+        sqlite_cursor.execute("SELECT * FROM original_content")
 
 def get_database_connection(database_path):
     conn = sqlite3.connect(database_path)
