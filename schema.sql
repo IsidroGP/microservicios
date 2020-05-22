2f153ea29f09377964e045a2f98b989ee38cf792
IsidroGP <32042452+IsidroGP@users.noreply.github.com>
2020-05-21 22:36:52 -0500

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "original_content" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"name"	TEXT NOT NULL,
-	"type"	TEXT NOT NULL,
-	"genre"	TEXT NOT NULL,
-	"imdb_rating"	REAL
-);
